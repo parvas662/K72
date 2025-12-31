738d0c7fb1340732c13acdb3e204032b6a5ffaae
Parvas662 <parvassharma662@gmail.com>
2026-01-01 01:07:18 +0530

    complete landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
+import HomeBottomText from "../components/home/HomeBottomText";
+import HomeHeroText from "../components/home/HomeHeroText";
+import Video from "../components/home/Video";
+
  
 const Home = () => {
   return (
     <div>
-      <h2 className="text-9xl text-black">Home</h2>
+      <div className="h-screen w-screen fixed">
+        <Video/> 
+      </div>
+      <div className="relative">
+        <HomeHeroText/>
+        <HomeBottomText/>
+      </div>
     </div>
   );
 };
