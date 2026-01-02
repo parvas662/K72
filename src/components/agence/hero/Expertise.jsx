92e5d0e82e9ae285e858491784fe84e9b9a7a6c8
Parvas662 <parvassharma662@gmail.com>
2026-01-03 01:01:41 +0530

    add agency-Expertise section

---
+++ src/components/agence/hero/Expertise.jsx
@@ -0,0 +1,29 @@
+ 
+const Expertise = () => {
+  return (
+    <div className="section2 font-semibold font-sans text-xl leading-6 my-[13vw] flex flex-col px-[8vw]">
+        <div className="flex gap-34 ">
+          <div className=" px-8 py-4">
+            Expertise
+          </div>
+          <ul className=" px-[6vw] py-4">
+            <li>Strategy</li>
+            <li>Advertisement</li>
+            <li>Branding</li>
+            <li>Design</li>
+            <li>Content</li>
+          </ul>
+        </div>
+        <div className="py-[10vw] px-8 ">
+          <ul className=" flex text-xl pr-12">
+            <li className="pr-8">Our projects_ are born in humility, grow in curiosity and live thanks to creativity in all its forms.</li>
+            <li className="px-6">Our creation_ is bubbling in an environment where talent wants to explode. Where you feel free to be the best version of yourself.</li>
+            <li className="">Our culture_ is openness to others. Point. The entire crew helps build an agency that we are proud of.</li>
+          </ul>
+
+        </div>
+      </div>
+  );
+};
+
+export default Expertise
